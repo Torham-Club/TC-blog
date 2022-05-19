22eee568c91cf191d65d7b178be9f8daa5db384b
torham <torhamdev@gmail.com>
2022-05-19 23:32:27 +0430

    Add models in django admin

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from blog.models import  UsersAdditionalInfo, Posts
 # Register your models here.
+
+admin.site.register(UsersAdditionalInfo)
+admin.site.register(Posts)
