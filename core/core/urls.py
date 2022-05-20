ce1482adc4305a05dd44ca3a6ca788bc58a90cda
Daniel_coder <68733224+DanielcoderX@users.noreply.github.com>
2022-05-20 19:09:55 +0430

    Static files served And Index(Home) page simply modified

--- core/core/urls.py
+++ core/core/urls.py
@@ -4,8 +4,7 @@ from rest_framework_simplejwt import views as jwt_views
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
-
+from . import views as coreview
 schema_view = get_schema_view(
    openapi.Info(
       title="Snippets API",
@@ -22,6 +21,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
+   path('', coreview.index,name="index"),
    path('admin/', admin.site.urls),
    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
