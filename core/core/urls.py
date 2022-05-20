0878430d701edaec686015161d2fb94e79c9164e
torham <torhamdev@gmail.com>
2022-05-20 23:02:40 +0430

    Update Swagger info

--- core/core/urls.py
+++ core/core/urls.py
@@ -8,12 +8,12 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="TC Blog APIs",
       default_version='v1',
-      description="Test description",
+      description="A blog where users can sign up and have their own posts and followers ",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      contact=openapi.Contact(email="TorhamDev@gmail.com"),
+      license=openapi.License(name="MIT License"),
    ),
    public=True,
    permission_classes=[permissions.AllowAny],
