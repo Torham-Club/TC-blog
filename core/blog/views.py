fa55c7d3196db88623b6d9c1a0a10a92ba606aec
pouya <p.pouya74@gmail.com>
2022-05-21 22:05:40 +0430

    added postcreate-api view

--- core/blog/views.py
+++ core/blog/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import generics
-from blog.serializers import UserSerializer, UpdateUserSerializer, UsersAdditionalInfoSerializer
+from blog.models import Posts
+from blog.serializers import PostCreateSerializer, UserSerializer, UpdateUserSerializer, UsersAdditionalInfoSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.throttling import UserRateThrottle
 
@@ -50,3 +51,15 @@ class CreateUsersAdditionalInfo(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
+
+class PostCreate(generics.CreateAPIView):
+    
+    queryset = Posts.objects.all()
+    serializer_class = PostCreateSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
