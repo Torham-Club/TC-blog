c8bf7d0ef462c37e965207ffe0f52c91e5833965
pouya <p.pouya74@gmail.com>
2022-05-21 22:05:08 +0430

    added postcreate-api url

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -4,5 +4,8 @@ from blog import views
 urlpatterns = [
     path('account/register', views.UserCreate.as_view()),
     path('account/update', views.UserUpdate.as_view()),
-    path('account/set/additional', views.CreateUsersAdditionalInfo.as_view())
+    path('account/set/additional', views.CreateUsersAdditionalInfo.as_view()),
+    # Post urls
+    path('create/', views.PostCreate.as_view(), name='post_create_api'),
+
 ]
