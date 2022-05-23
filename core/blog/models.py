0f4c83ede20062285bbf3ec0ff730bc60bb36255
pouya <p.pouya74@gmail.com>
2022-05-23 23:00:43 +0430

    added Comment model

--- core/blog/models.py
+++ core/blog/models.py
@@ -52,3 +52,22 @@ class Posts(models.Model):
 
     def __str__(self) -> str:
         return f"{self.author.username} | {self.title}"
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Posts,
+                        on_delete=models.CASCADE,
+                        related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+
+    class Meta:
+        ordering = ('created',)
+    
+    def __str__(self)->str:
+        return f'Comment by {self.name} on {self.post}'
